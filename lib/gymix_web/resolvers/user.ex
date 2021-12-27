86fa89ad54220ed259be12f5e1b24c1b40385559
Joao Gomes <joaopfg.2002@gmail.com>
2021-12-27 21:01:54 +0000

    Create and Get Users with graphiql

--- lib/gymix_web/resolvers/user.ex
+++ lib/gymix_web/resolvers/user.ex
@@ -1,3 +1,4 @@
 defmodule GymixWeb.Resolvers.User do
+  def create(%{input: params}, _context), do: Gymix.User.Create.call(params)
   def get(%{id: user_id}, _context), do: Gymix.User.Get.call(user_id)
 end
